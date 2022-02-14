2739cec9dff8402f73f8c4ddfc62e15a0bd899dc
jashanjashan <97339122+jashanjashan@users.noreply.github.com>
2022-02-14 14:45:57 +0530

    Add files via upload

---
+++ assignment3ITCoutput.py
@@ -0,0 +1,91 @@
+#q(1)
+============= RESTART: C:\Users\dell\Desktop\assignment3 ques 1.py =============
+Enter  string: we will we will rock you
+{'we': 2, 'will': 2, 'rock': 1, 'you': 1}
+ 
+
+#q2
+enter date: 28
+input month: 2
+enter year: 1999
+1 3 1999
+
+
+
+#q3
+
+[(3, 9), (9, 81), (10, 100)]
+
+
+#q4
+enter the grade point:9
+Your Grade is 'A' and Excellent performance
+
+#q5
+ABCDEFGHIJK
+ ABCDEFGHI
+  ABCDEFG
+   ABCDE
+    ABC
+     A
+
+#q6
+a part
+enter no of students: 2
+enter the sid: 21103087
+enter the namejashan
+enter the sid: 21103088
+enter the nametejvir
+{21103087: 'jashan', 21103088: 'tejvir'}
+now let us discuss b part
+{21103087: 'jashan', 21103088: 'tejvir'}
+let us discuss c part
+{21103087: 'jashan', 21103088: 'tejvir'}
+let us discuss d part
+enter one of SID you entered before:21103087
+jashan
+
+
+#q7
+
+
+================ RESTART: C:\Users\dell\Desktop\assign3ques7.py ================
+enter first number:3
+enter second number:4
+number  of extra terms we want to add in fibonacci series apart from first two :3
+[3, 4, 7, 11, 18] average_of_series 8.6
+
+#q(8a)
+{1, 3, 5, 6, 8}
+
+#q(8b)
+
+=============== RESTART: C:\Users\dell\Desktop\assign3ques8(b).py ==============
+{17, 3, 6, 8, 9, 13}
+# q(8c)
+{1, 2, 4, 5}
+
+#q(8d)
+{8, 9, 6, 7}
+#q8e
+{7}
+
+
+
+
+
+
+                            
+
+
+
+
+
+  
+     
+            
+            
+
+
+        
+                
